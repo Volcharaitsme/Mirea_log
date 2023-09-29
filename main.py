abede0eef6506bef14c4f4166543151e891ca053
danik <danik.prigo@gmail.com>
2023-09-29 20:16:44 +0300

    adding alchemy and all basic

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+from routers.items import router as item_router
+
+app = FastAPI()
 
 
-print ('Hello wolrd!')
+app.include_router(
+    router=item_router,
+    prefix='/items',
+)
